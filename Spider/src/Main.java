ecf530160900aa4ef03cc3374f2f190e027bad4e
sanderc7 <sanderc7@wwu.edu>
2019-02-16 13:29:18 -0800

    pageReader now parses each line; implementing feature to find all links

--- Spider/src/Main.java
+++ Spider/src/Main.java
@@ -6,13 +6,19 @@ import java.util.*;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import com.gargoylesoftware.htmlunit.WebClient;
 
 public class Main {
     public static void main(String[] args) {
         PageReader read = new PageReader();
-        read.pageReader();
-        //WebClient client = new WebClient();
+        String startingWebPage = "file:///home/sanderc7/WebCrawler/WebCrawler/TestSite.html";
+        //String startingWebPage = "https://www.youtube.com/";
+        //String startingWebPage = "https://www.youtube.com/yt/about/press/";
+        try{
+            read.pageReader(startingWebPage);
+        } catch (IOException e){
+            System.out.println("Can't read given URL");
+        }
+
 
     }
 }
--- Spider/src/PageReader.java
+++ Spider/src/PageReader.java
@@ -6,9 +6,11 @@ import java.util.*;
 import java.net.URL;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 public class PageReader {
-    public static void pageReader() throws IOException{
-        String urlString = "https://www.youtube.com/robots.txt";
+     public void pageReader(String urlString) throws IOException{
+
+        //String urlString = "https://stackoverflow.com/sitemap.xml";
         URL url;
         try {
             url = new URL(urlString);
@@ -19,10 +21,27 @@ public class PageReader {
         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
 
         String line;
+        //12
+         int counter = 0;
         while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            //System.out.println(line);
+            String[] splitLine = line.split("\"");
+            counter++;
+            //System.out.println(counter);
+            for (int i = 0; i < splitLine.length; i++){
+                System.out.println(splitLine[i]);
+                if (splitLine[i].equals("")){
+                    System.out.println("yessir");
+                }
+//                char c = splitLine[i].charAt(0);
+//                if(c == 104){
+//                    System.out.println("first try baby");
+//                }
+
+            }
         }
 
         reader.close();
     }
+
 }
