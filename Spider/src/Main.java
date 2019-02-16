a7df5687d8433a905701bee136adb75c75a3af45
smithe68 <smithe68@wwu.edu>
2019-02-16 10:56:02 -0800

    added needed library

---
+++ Spider/src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by smithe68 on 2/16/19.
+ */
+
+import com.gargoylesoftware.htmlunit.WebClient;
+public class Main {
+    public static void main(String[] args) {
+        WebClient client = new WebClient();
+    }
+}
--- Spider/src/Spider.java
+++ Spider/src/Spider.java
@@ -2,4 +2,5 @@
  * Created by smithe68 on 2/16/19.
  */
 public class Spider {
+
 }
