98d341da7c7508753a19bb0a6f95ca401fd0ecd4
sanderc7 <sanderc7@wwu.edu>
2019-02-16 18:40:57 -0800

    last push before the great merge

--- Spider/src/Website.java
+++ Spider/src/Website.java
@@ -1,11 +1,12 @@
 /**
  * Created by smithe68 on 2/16/19.
  */
+import java.util.LinkedList;
 public class Website {
     private String name = "";
     private int inLinks = 1; //number of pages which contain a link to this website
     private int outLinks = 0;
-
+    private LinkedList<String> adjacentPages = new LinkedList<String>();
     public String getName() {
         return name;
     }
